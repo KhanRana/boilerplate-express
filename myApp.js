ca42c151920c4b84a6ce4c7595566cc29e76c90f
KhanRana <47982799+KhanRana@users.noreply.github.com>
2024-02-09 14:36:37 +0000

    add styling

--- myApp.js
+++ myApp.js
@@ -3,10 +3,14 @@ let app = express();
 
 console.log("Hello World");
 
+app.use(express.static(__dirname + '/public'));
+
+
 app.get('/', function(req, res) {
     // res.send("Hello Express");
     res.sendFile(__dirname + '/views/index.html')
 });
+
 
 
 
